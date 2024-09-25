c6f6706255701ee5a9ae646959fabc7430d97b2f
D0kDoom <D0kDoom@protonmail.com>
2024-09-25 15:08:35 -0300

    Bare bones version

--- arduinoSDAA/arduinoSDAA.ino
+++ arduinoSDAA/arduinoSDAA.ino
@@ -1,21 +1,27 @@
 //código simples, lê os valores do giroscópio e usa como parâmetro para enviar uma mensagem
 //usa um giroscópio MPU-6050, um Arduino Uno e um módulo HC-05
+//compilou de primeira, nunca fui tão feliz <3
 //coded by D0kDoom
-//My Github: https://github.com/D0kDoom
+//my Github: https://github.com/D0kDoom
 
+//declaração de bibliotecas
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <MPU6050_tockn.h>
 
+//constantes para os pinos RX TX
 const int blueRX = 2;
 const int blueTX = 3;
 
+//constantes que definem a condição de ativação
 const int tgtX = 45;
 const int tgtY = 20;
 const int tgtZ = 30;
 
+//define se um acidente aconteceu ou não
 bool acc = false;
 
+//latitude e longitude da sala de robótica no SESI, mudar de acordo com localização
 String lat = "Latitude: 22 7 19 S";
 String longi = "Longitude: 43 12 36 W";
 
@@ -24,7 +30,8 @@ SoftwareSerial SerialBT(blueRX, blueTX);
 MPU6050 Gyro(Wire);
 
 void setup() {
-  
+
+  //inicialização das seriais e wire
   Serial.begin(9600);
   SerialBT.begin(9600);
   Wire.begin();
@@ -43,15 +50,18 @@ void setup() {
 
 void loop() {
 
+  //recebe os valores de ângulo do giroscópio, envia os valores para as variáveis locais
   Gyro.update();
   double angleX = Gyro.getAngleX();
   double angleY = Gyro.getAngleY();
   double angleZ = Gyro.getAngleZ();
 
+  //faz a comparação dos ângulos e define se um acidente ocorreu
   if (angleX >= tgtX||angleX <= -tgtX||angleY >= tgtY||angleY <= -tgtY||angleZ >= tgtZ||angleZ <= -tgtZ) {
     
     acc = true;
-    
+
+    //envia o aviso junto com a geolocalização para o bluetooth
     SerialBT.println("Oh Noes, um acidente foi detectado");
     SerialBT.println("Enviando localização...");
     SerialBT.println(lat);
@@ -59,6 +69,7 @@ void loop() {
 
   }
 
+  //função que permite o reset manual do sistema sem a necessidade de reiniciar o Arduino fisicamente
   while(acc = true) {
       if (SerialBT.available()) {
         char reset = SerialBT.read();
